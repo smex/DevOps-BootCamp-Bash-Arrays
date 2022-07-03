17adc00cac11391b672d811ffb9af47153a1ac4c
VALERY_BUDICH <valery.budich@idp.zyfra.com>
2022-07-03 23:58:20 +0300

    first release

--- array.sh
+++ array.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-# Place your code here
+num=$1
+for idx in {a..z}
+do
+    echo $num
+    if [ $num -le 0 ]; then
+        break
+    fi
+    let "num-=1"
+
+    mkdir "folder_$idx"
+done
